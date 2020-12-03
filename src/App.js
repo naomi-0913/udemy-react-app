607c81c632436d7742ecf9f81d768669c9a8b0b7
naomi-0913 <upworld409019@gmail.com>
2020-12-03 22:03:29 +0900

    create my own React boilerplate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-	<h1 ClassName="App-title">Hello World</h1>
-      </header>
-    </div>
-  );
-}
+  return <div><h1 ClassName="App-title">Hello World</h1></div>
+};
 
 export default App;
